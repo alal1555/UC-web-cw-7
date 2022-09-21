d4d5f3cfc072de50c9fa9724494fa64f2e0198a9
alal1555 <alal1555@hotmail.com>
2022-09-21 19:43:14 +0300

    cw-7

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ let random_grade_div = document.getElementById("random-grade");
 
 
 /*  الجزء الأول والثاني من التمرين */
+grades.pop();
+grades.push(99);
 
+grades.forEach(item => {
+  all_grades_div.innerHTML += `<span>${item}<span>`
+});
 
-
-
+randomIndex = Math.floor(Math.random() * grades.length)
+random_grade_div.innerHTML = `<span>#${randomIndex}: ${grades[randomIndex]}<span>`
 
 
 
@@ -26,12 +31,37 @@ let course = {
 }
 
 //Change studentsCount,track property here
+course.studentsCount=40;
+course.track="web";
 
 
 
 //Console log here
-
+console.log("Hello my name is "+course.name+", i'm in "+course.track +" at "+course.location)
 
 
 //Create student object here
+let student ={
+  name:"Aly",
+  university:"AOU"
+}
 
+
+let unicodes=[
+  {
+    track:"web",
+    location:"AOU"
+  },
+  {
+    track:"Android",
+    location:"Kuwait University"
+  },
+  {
+    track:"Games",
+    location:"Cairo University"
+  }
+];
+
+unicodes.forEach(element => {
+  console.log(element.track)
+});
